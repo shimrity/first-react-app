baa22d15057d1d59b68d25a61f658878db3fbf8c
shimrity <shimrit.moryossef@gmail.com>
2020-07-22 13:55:40 +0300

    GuessNumber

--- src/main.js
+++ src/main.js
@@ -1,26 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Text from './Text';
+
 import { useState } from 'react';
 
 import '../css/main.css';
 
 const App = () => {
-  const [valu, setVale] = useState(0);
-  function inc(val,f)
-  {
-    //alert(val);
-    valu =  val/f;
-    setVale(valu);
-  }
+  const [isTimeToLay, setIsTimeToLayVal] = useState(false);
+  const [Msg, setMsgVal] = useState("");
+  const [RandomNum ,setRandomNum] = useState(Math.floor(Math.random() * 1000) + 1);
   
+  setTimeout(function(){
+    setIsTimeToLayVal(x => !x)
+   },5000);
+  
+
+  function TryGuess(e)
+  {
+    const elm = document.querySelector('.GuessNumber');
+    const GuessNum=Number(elm.value);
+    debugger;
+    if(GuessNum==RandomNum)
+      setMsgVal("You Did It!!!!!");// ==> "+ RandomNum);
+    else
+    {
+        if(GuessNum>RandomNum)
+        {
+          if(!isTimeToLay)
+            setMsgVal("Too Big ");//+ RandomNum + "------" + isTimeToLay);
+         else
+            setMsgVal("Too Small ");//+ RandomNum + "-----" + isTimeToLay);
+        }
+        else
+        {
+          if(isTimeToLay)
+            setMsgVal("Too Big ");//+ RandomNum + "------" + isTimeToLay);
+         else
+            setMsgVal("Too Small ");//+ RandomNum + "-----" + isTimeToLay);
+        }
+
+    }  
+
+  }
+  function Restart(e)
+  {
+    document.querySelector('.GuessNumber').value ='';    
+    setRandomNum(Math.floor(Math.random() * 1000) + 1);
+    setMsgVal("");
+    setisTimeToLayVal(false)
+  }
   return (
     <div>
-      <h1>Enter time:</h1>
-     <Text val={valu} inc={inc} factor={1}/> <br/>
-     <Text val={valu} inc={inc} factor={60}/> <br/>
-     <Text val={valu} inc={inc} factor={3600}/> <br/>    
-     
+      <p>Guess Number:
+      <input type="number" className="GuessNumber"></input> 
+      <button onClick={TryGuess}>TryGuess</button></p>
+      <label>{Msg}</label>
+      <br/>
+      <button onClick={Restart}>Restart</button>
     </div>
   )
 };
