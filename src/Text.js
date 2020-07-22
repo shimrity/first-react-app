5b11068124c1502871319172aad7379120aacbcf
shimrity <shimrit.moryossef@gmail.com>
2020-07-22 09:28:33 +0300

    timer

--- src/Text.js
+++ src/Text.js
@@ -2,15 +2,16 @@ import React from 'react';
 
 export default function Text(prop)
 {
-    const {inc ,val} = prop;
+    const {inc ,val , factor} = prop;
     function handleChangeVal(e) {
-        const newValue = (e.target.value);
-       // alert(e.target.value)
-         inc(newValue);
+        const newValue = (e.target.value) ;
+       // alert (newValue);
+         //let timer = number(newValue) / factor
+         inc(newValue,factor);
       }
 return (
     <>
-        <input type="text" value={val} onChange={handleChangeVal} />   
+        <input type="number" value={val} onChange={handleChangeVal} />   
     </>
  );
 
--- src/main.js
+++ src/main.js
@@ -6,20 +6,20 @@ import { useState } from 'react';
 import '../css/main.css';
 
 const App = () => {
-  const [valu, setVale] = useState("");
-  function inc(val)
+  const [valu, setVale] = useState(0);
+  function inc(val,f)
   {
-    setVale(val) 
+    //alert(val);
+    valu =  val/f;
+    setVale(valu);
   }
   
   return (
     <div>
-      <h1>Enter text:</h1>
-     <Text val={valu} inc={inc}/> <br/>
-     <Text val={valu} inc={inc}/> <br/>
-     <Text val={valu} inc={inc}/> <br/>
-     <Text val={valu} inc={inc}/>
-     
+      <h1>Enter time:</h1>
+     <Text val={valu} inc={inc} factor={1}/> <br/>
+     <Text val={valu} inc={inc} factor={60}/> <br/>
+     <Text val={valu} inc={inc} factor={3600}/> <br/>    
      
     </div>
   )
